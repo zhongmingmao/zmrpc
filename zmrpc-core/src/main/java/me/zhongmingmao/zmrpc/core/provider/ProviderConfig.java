034cc4c3db24ccad583ca9f629952828e4bfbf70
zhongmingmao <zhongmingmao0625@gmail.com>
2024-08-06 23:49:53 +0800

    feat(registry): add curator

--- zmrpc-core/src/main/java/me/zhongmingmao/zmrpc/core/provider/ProviderConfig.java
+++ zmrpc-core/src/main/java/me/zhongmingmao/zmrpc/core/provider/ProviderConfig.java
@@ -1,5 +1,7 @@
 package me.zhongmingmao.zmrpc.core.provider;
 
+import me.zhongmingmao.zmrpc.core.api.RegistryCenter;
+import me.zhongmingmao.zmrpc.core.registry.ZkRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,5 +11,11 @@ public class ProviderConfig {
   @Bean
   ProviderBootstrap providerBootstrap() {
     return new ProviderBootstrap();
+  }
+
+  // 定义注册中心的启动关闭钩子
+  @Bean(initMethod = "start", destroyMethod = "stop")
+  public RegistryCenter registryCenter() {
+    return new ZkRegistry();
   }
 }
--- zmrpc-core/src/main/java/me/zhongmingmao/zmrpc/core/registry/ZkRegistry.java
+++ zmrpc-core/src/main/java/me/zhongmingmao/zmrpc/core/registry/ZkRegistry.java
@@ -1,21 +1,74 @@
 package me.zhongmingmao.zmrpc.core.registry;
 
-import me.zhongmingmao.zmrpc.core.api.RegistryCenter;
-
 import java.util.List;
+import me.zhongmingmao.zmrpc.core.api.RegistryCenter;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
 
 public class ZkRegistry implements RegistryCenter {
-  @Override
-  public void start() {}
+
+  private CuratorFramework client = null;
 
   @Override
-  public void stop() {}
+  public void start() {
+    RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+    client =
+        CuratorFrameworkFactory.builder()
+            .connectString("127.0.0.1:2181")
+            .namespace("zmrpc")
+            .retryPolicy(retryPolicy)
+            .build();
+  }
 
   @Override
-  public void register(String service, String instance) {}
+  public void stop() {
+    client.close();
+  }
 
   @Override
-  public void unregister(String service, String instance) {}
+  public void register(String service, String instance) {
+    String servicePath = "/" + service;
+
+    try {
+      // service 注册为持久化节点
+      if (client.checkExists().forPath(servicePath) == null) {
+        client.create().withMode(CreateMode.PERSISTENT).forPath(servicePath, "service".getBytes());
+      }
+
+      // instance 注册为临时节点 - 不断变化
+      String instancePath = servicePath + "/" + instance;
+      if (client.checkExists().forPath(instancePath) == null) {
+        client.create().withMode(CreateMode.EPHEMERAL).forPath(instancePath, "provider".getBytes());
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void unregister(String service, String instance) {
+    String servicePath = "/" + service;
+
+    try {
+      if (client.checkExists().forPath(servicePath) == null) {
+        // service 不存在，直接返回
+        return;
+      }
+
+      String instancePath = servicePath + "/" + instance;
+      if (client.checkExists().forPath(instancePath) == null) {
+        // instance 不存在，直接返回
+        return;
+      }
+
+      client.delete().quietly().forPath(instancePath); // 删除 instance
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   @Override
   public List<String> fetchAll(String service) {
