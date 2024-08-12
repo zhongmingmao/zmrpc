496e1c31f9a4e619663f1efbc1e15964b071e8d6
zhongmingmao <zhongmingmao0625@gmail.com>
2024-08-12 21:01:29 +0800

    refactor(ci): support ut for consumer

--- zmrpc-demo-consumer/src/test/java/me/zhongmingmao/zmrpc/demo/consumer/ZmrpcDemoConsumerApplicationTests.java
+++ zmrpc-demo-consumer/src/test/java/me/zhongmingmao/zmrpc/demo/consumer/ZmrpcDemoConsumerApplicationTests.java
@@ -1,10 +1,31 @@
 package me.zhongmingmao.zmrpc.demo.consumer;
 
+import me.zhongmingmao.zmrpc.demo.provider.ZmrpcDemoProviderApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class ZmrpcDemoConsumerApplicationTests {
+
+  static ConfigurableApplicationContext context;
+
+  @BeforeAll
+  static void init() {
+    context =
+        SpringApplication.run(
+            ZmrpcDemoProviderApplication.class,
+            "--server.port=8084",
+            "--logging.level.me.zhongmingmao.zmrpc=debug");
+  }
+
+  @AfterAll
+  static void destroy() {
+    SpringApplication.exit(context);
+  }
 
   @Test
   void contextLoads() {}
